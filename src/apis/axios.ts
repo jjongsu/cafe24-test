f02521a53eeaa0ac7a084774f5db86d6d688d4b8
황종수 <jongsuhwang@hansol.com>
2024-10-21 16:37:10 +0900

    ✨ Feat : accessToken test

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,9 +1,23 @@
-// import axios from 'axios';
-// import { makeGet, makePatch, makePost } from './utils';
+import axios from 'axios';
+// import { makeGet, makePatch, makePost } from '@/lib/utils';
+import { makePost } from '@/lib/utils';
 
-// const client = axios.create({
-//     baseURL: 'https://hjs1002.cafe24api.com/',
-// });
+export const accessTokenAxios = axios.create({
+    baseURL: 'https://hjs1002.cafe24api.com/',
+    headers: {
+        Authorization: 'Basic TFdmVjVmQm0xRUoxdUtmUHdzT0o1QSA6IDNxNHJPMHVKT1J0aXY2UTdtNEoxYUQ=',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+});
+
+export const makePostAccessToken = makePost(accessTokenAxios);
+
+export const postAccessToken = (body: { code: string }) =>
+    makePostAccessToken(`api/v2/oauth/token`, {
+        grant_type: 'authorization_code',
+        code: body.code,
+        redirect_uri: 'https://cafe24-hjs1002.vercel.app',
+    });
 
 // const makeGetTI = makeGet(client);
 // const makePostTI = makePost(client);
--- src/pages/board/Board.tsx
+++ src/pages/board/Board.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useRef } from 'react';
+import { postAccessToken } from '@apis/axios';
+import { useMutation } from '@tanstack/react-query';
+import { useCallback, useEffect, useRef } from 'react';
+// import { useNavigate } from 'react-router-dom';
 
 const findHref = (ref: React.RefObject<HTMLAnchorElement>) => {
     const url = new URL(window.location.href);
     const urlParams = url.searchParams;
-    console.log('findHref');
+    console.log('findHref', urlParams);
     const mallId = urlParams.get('mall_id');
     if (mallId && ref.current) {
         const tokenUrl =
@@ -17,12 +20,27 @@ const findHref = (ref: React.RefObject<HTMLAnchorElement>) => {
         ref.current.href = tokenUrl;
     }
 };
+
 export default function Board() {
     const ATagRef = useRef<HTMLAnchorElement>(null);
+    // const navigate = useNavigate();
+    const { mutate } = useMutation({ onSuccess: () => console.log('success'), mutationFn: postAccessToken });
+
+    const hasCode = useCallback(() => {
+        const url = new URL(window.location.href);
+        const urlParams = url.searchParams;
+        console.log('hasCode', urlParams);
+        const code = urlParams.get('code');
+        if (code) {
+            // navigate('/board', { replace: true });
+            mutate({ code });
+        }
+    }, [mutate]);
 
     useEffect(() => {
         findHref(ATagRef);
-    }, []);
+        hasCode();
+    }, [hasCode]);
 
     return (
         <p id='app'>
